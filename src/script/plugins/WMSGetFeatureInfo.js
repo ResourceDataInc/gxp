b90e35b598b2f4a9bdf61718578fd82519583a36
Cory Smith <corys@resdat.com>
2016-03-09 13:46:44 -0900

    modify FeatureInfo box behavior to prevent mulitple boxes

--- src/script/plugins/WMSGetFeatureInfo.js
+++ src/script/plugins/WMSGetFeatureInfo.js
@@ -11,6 +11,7 @@
  * @requires plugins/FeatureEditorGrid.js
  * @requires GeoExt/widgets/Popup.js
  * @requires OpenLayers/Control/WMSGetFeatureInfo.js
+ * @requires OpenLayers/Format/GeoJSON.js
  * @requires OpenLayers/Format/WMSGetFeatureInfo.js
  */
 
@@ -30,12 +31,12 @@ Ext.namespace("gxp.plugins");
  *    This plugins provides an action which, when active, will issue a
  *    GetFeatureInfo request to the WMS of all layers on the map. The output
  *    will be displayed in a popup.
- */   
+ */
 gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
-    
+
     /** api: ptype = gxp_wmsgetfeatureinfo */
     ptype: "gxp_wmsgetfeatureinfo",
-    
+
     /** api: config[outputTarget]
      *  ``String`` Popups created by this tool are added to the map by default.
      */
@@ -57,12 +58,12 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
      *  Title for info popup (i18n).
      */
     popupTitle: "Feature Info",
-    
+
     /** api: config[text]
      *  ``String`` Text for the GetFeatureInfo button (i18n).
      */
     buttonText: "Identify",
-    
+
     /** api: config[format]
      *  ``String`` Either "html" or "grid". If set to "grid", GML will be
      *  requested from the server and displayed in an Ext.PropertyGrid.
@@ -70,18 +71,18 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
      *  Default is "html".
      */
     format: "html",
-    
+
     /** api: config[vendorParams]
      *  ``Object``
      *  Optional object with properties to be serialized as vendor specific
      *  parameters in the requests (e.g. {buffer: 10}).
      */
-    
+
     /** api: config[layerParams]
      *  ``Array`` List of param names that should be taken from the layer and
      *  added to the GetFeatureInfo request (e.g. ["CQL_FILTER"]).
      */
-     
+
     /** api: config[itemConfig]
      *  ``Object`` A configuration object overriding options for the items that
      *  get added to the popup for each server response or feature. By default,
@@ -95,11 +96,18 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
      *      html: text, // responseText from server - only for "html" format
      */
 
+    /** private: method[constructor]
+     */
+    constructor: function (config) {
+        this.geoJSONFormat = new OpenLayers.Format.GeoJSON();
+        gxp.plugins.WMSGetFeatureInfo.superclass.constructor.apply(this, arguments);
+    },
+
     /** api: method[addActions]
      */
-    addActions: function() {
+    addActions: function () {
         this.popupCache = {};
-        
+
         var actions = gxp.plugins.WMSGetFeatureInfo.superclass.addActions.call(this, [{
             tooltip: this.infoActionTip,
             iconCls: "gxp-icon-getfeatureinfo",
@@ -107,38 +115,38 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
             toggleGroup: this.toggleGroup,
             enableToggle: true,
             allowDepress: true,
-            toggleHandler: function(button, pressed) {
-                for (var i = 0, len = info.controls.length; i < len; i++){
+            toggleHandler: function (button, pressed) {
+                for (var i = 0, len = info.controls.length; i < len; i++) {
                     if (pressed) {
                         info.controls[i].activate();
                     } else {
                         info.controls[i].deactivate();
                     }
                 }
-             }
+            }
         }]);
         var infoButton = this.actions[0].items[0];
 
-        var info = {controls: []};
-        var updateInfo = function() {
-            var queryableLayers = this.target.mapPanel.layers.queryBy(function(x){
+        var info = { controls: [] };
+        var updateInfo = function () {
+            var queryableLayers = this.target.mapPanel.layers.queryBy(function (x) {
                 return x.get("queryable");
             });
 
             var map = this.target.mapPanel.map;
             var control;
-            for (var i = 0, len = info.controls.length; i < len; i++){
+            for (var i = 0, len = info.controls.length; i < len; i++) {
                 control = info.controls[i];
                 control.deactivate();  // TODO: remove when http://trac.openlayers.org/ticket/2130 is closed
                 control.destroy();
             }
 
             info.controls = [];
-            queryableLayers.each(function(x){
+            queryableLayers.each(function (x) {
                 var layer = x.getLayer();
                 var vendorParams = Ext.apply({}, this.vendorParams), param;
                 if (this.layerParams) {
-                    for (var i=this.layerParams.length-1; i>=0; --i) {
+                    for (var i = this.layerParams.length - 1; i >= 0; --i) {
                         param = this.layerParams[i].toUpperCase();
                         vendorParams[param] = layer.params[param];
                     }
@@ -156,41 +164,28 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
                     infoFormat: infoFormat,
                     vendorParams: vendorParams,
                     eventListeners: {
-                        getfeatureinfo: function(evt) {
+                        getfeatureinfo: function (evt) {
                             var title = x.get("title") || x.get("name");
                             if (infoFormat == "text/html") {
                                 var match = evt.text.match(/<body[^>]*>([\s\S]*)<\/body>/);
                                 if (match && !match[1].match(/^\s*$/)) {
                                     this.displayPopup(evt, title, match[1]);
                                 } else {
-                                    var toProjection = new OpenLayers.Projection("EPSG:4326");
-                                    var position = map.getLonLatFromPixel(evt.xy).transform(map.getProjectionObject(), toProjection);
-                                    var popup = new GeoExt.Popup({
-                                        title: "No features found",
-                                        location: evt.xy,
-                                        map: map,
-                                        html: "Lat: " + position.lat + "<br/> Lon: " + position.lon,
-                                        width: 200,
-                                        collapsible: true
-                                    });
-                                    popup.show();
+                                    this.displayPopup(evt, title);
                                 }
                             } else if (infoFormat == "text/plain") {
                                 this.displayPopup(evt, title, '<pre>' + evt.text + '</pre>');
                             } else if (evt.features && evt.features.length > 0) {
-                                this.displayPopup(evt, title, null,  x.get("getFeatureInfo"));
+                                this.displayPopup(evt, title, null, x.get("getFeatureInfo"));
+                            } else if (infoFormat === 'application/json' && evt.text) {
+                                evt.features = this.geoJSONFormat.read(evt.text);
+                                if (evt.features && evt.features.length > 0) {
+                                    this.displayPopup(evt, title, null, x.get("getFeatureInfo"));
+                                } else {
+                                    this.displayPopup(evt, title);
+                                }
                             } else {
-                                var toProjection = new OpenLayers.Projection("EPSG:4326");
-                                var position = map.getLonLatFromPixel(evt.xy).transform(map.getProjectionObject(), toProjection);
-                                var popup = new GeoExt.Popup({
-                                    title: "No features found",
-                                    location: evt.xy,
-                                    map: map,
-                                    html: "Lat: " + position.lat + "<br/> Lon: " + position.lon,
-                                    width: 200,
-                                    collapsible: true
-                                });
-                                popup.show();
+                                this.displayPopup(evt, title);
                             }
                         },
                         scope: this
@@ -198,17 +193,17 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
                 }, this.controlOptions));
                 map.addControl(control);
                 info.controls.push(control);
-                if(infoButton.pressed) {
+                if (infoButton.pressed) {
                     control.activate();
                 }
             }, this);
 
         };
-        
+
         this.target.mapPanel.layers.on("update", updateInfo, this);
         this.target.mapPanel.layers.on("add", updateInfo, this);
         this.target.mapPanel.layers.on("remove", updateInfo, this);
-        
+
         return actions;
     },
 
@@ -219,21 +214,22 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
      *     reporting the info to the user
      * :arg text: ``String`` Body text.
      */
-    displayPopup: function(evt, title, text, featureinfo) {
+    displayPopup: function (evt, title, text, featureinfo) {
         var popup;
         var popupKey = evt.xy.x + "." + evt.xy.y;
+        var map = this.target.mapPanel.map;
+        var position = map.getLonLatFromPixel(evt.xy).transform(map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"));
         featureinfo = featureinfo || {};
         if (!(popupKey in this.popupCache)) {
             popup = this.addOutput({
                 xtype: "gx_popup",
-                title: this.popupTitle,
+                title: this.popupTitle + ": " + position.lat + ", " + position.lon,
                 layout: "accordion",
                 fill: false,
                 autoScroll: true,
                 location: evt.xy,
                 map: this.target.mapPanel,
-                width: 250,
-                height: 300,
+                width: 400,
                 defaults: {
                     layout: "fit",
                     autoScroll: true,
@@ -242,9 +238,9 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
                     collapsible: true
                 }
             });
-            popup.on({                    
-                close: (function(key) {
-                    return function(panel){
+            popup.on({
+                close: (function (key) {
+                    return function (panel) {
                         delete this.popupCache[key];
                     };
                 })(popupKey),
@@ -258,7 +254,7 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
         var features = evt.features, config = [];
         if (!text && features) {
             var feature;
-            for (var i=0,ii=features.length; i<ii; ++i) {
+            for (var i = 0, ii = features.length; i < ii; ++i) {
                 feature = features[i];
                 config.push(Ext.apply({
                     xtype: "gxp_editorgrid",
@@ -283,7 +279,7 @@ gxp.plugins.WMSGetFeatureInfo = Ext.extend(gxp.plugins.Tool, {
         popup.add(config);
         popup.doLayout();
     }
-    
+
 });
 
 Ext.preg(gxp.plugins.WMSGetFeatureInfo.prototype.ptype, gxp.plugins.WMSGetFeatureInfo);
